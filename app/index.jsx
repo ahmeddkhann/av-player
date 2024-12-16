bad95132769684f14bf4f180d872f33e5bb6ae50
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-12-16 18:48:47 +0500

    play stop and seek audio functions added

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import { Audio, Video } from 'expo-av'
 
 const App = () => {
   const audios = [
@@ -16,6 +17,47 @@ const App = () => {
     'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
     'https://sample-videos.com/video123/mp4/480/asdasdas.mp4',
   ]
+
+  const audioPlayer = useRef(new Audio.Sound())  
+  const videoRef = useRef (null);
+
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [currentAudioIndex, setCurrentAudioIndex] = useState(0);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [audioDuration, setAudioDuration] = useState();
+  const [audioPosition, setAudioPosition] = useState();
+
+ const playAudio = async () => {
+  try {
+    await audioPlayer.current.unloadAsync();
+    await audioPlayer.current.loadAsync({uri: audios[currentAudioIndex]});
+    await audioPlayer.current.unloadAsync();
+    setIsAudioPlaying(true)
+    
+  } catch (error) {
+    console.log("Error while playing the audio: ", error);
+  }
+ }
+
+ const stopAudio = async () => {
+  try {
+    await audioPlayer.current.stopAsync()
+    setIsAudioPlaying(false)
+    
+  } catch (error) {
+    console.log("error while stopping the Audio: ", error);
+  }
+ }
+
+const seekAudio = async (value) => {
+  try {
+    audioPlayer.current.setPositionAsync(value);
+    setAudioPosition(value);
+  } catch (error) {
+    console.log("error while changing position of Audio: ", error);
+  }
+}
+
   
   return (
     <View>
